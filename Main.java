b447402f2edbddc46fba3d042d3be432819cc3d0
Behrang <Behrang Saeedzadeh>
2014-12-07 18:38:41 +1100

    Blah

--- Main.java
+++ Main.java
@@ -1,39 +1,46 @@
 import java.util.*;
+import java.io.*;
+import java.nio.file.*;
+
 
 public class Main {
-	public static void main(String[] args) {
-		List<Integer> numbers = new ArrayList<Integer>();
-		Random rand = new Random();
+	public static void main(String[] args) throws Exception {
+		List<Object> blobs = new ArrayList<Object>();		
 		
 		int count = 1;
+
 		while (true) {
-			numbers.add(rand.nextInt());
-			if (count++ % 10000 == 0) {
-
-				System.out.println(numbers.size() + "\n\n\n====\n\n\n");
-
-				int mb = 1024*1024;
-         
-		        //Getting the runtime reference from system
-		        Runtime runtime = Runtime.getRuntime();
-		         
-		        System.out.println("##### Heap utilization statistics [MB] #####");
-		         
-		        //Print used memory
-		        System.out.println("Used Memory:"
-		            + (runtime.totalMemory() - runtime.freeMemory()) / mb);
-		 
-		        //Print free memory
-		        System.out.println("Free Memory:"
-		            + runtime.freeMemory() / mb);
-		         
-		        //Print total available memory
-		        System.out.println("Total Memory:" + runtime.totalMemory() / mb);
-		 
-		        //Print Maximum available memory
-		        System.out.println("Max Memory:" + runtime.maxMemory() / mb);
-
+			blobs.add(loadFile());
+			if (++count % 50 == 0) {
+				printStatistics();
 			}
 		}
+	}
+
+	public static Object loadFile() throws Exception {		
+		return Files.readAllBytes(Paths.get(new File("large.blob").toURI()));
+	}
+
+	public static void printStatistics() {
+		int mb = 1024*1024;
+         
+        //Getting the runtime reference from system
+        Runtime runtime = Runtime.getRuntime();
+         
+        System.out.println("##### Heap utilization statistics [MB] #####");
+         
+        //Print used memory
+        System.out.println("Used Memory:"
+            + (runtime.totalMemory() - runtime.freeMemory()) / mb);
+ 
+        //Print free memory
+        System.out.println("Free Memory:"
+            + runtime.freeMemory() / mb);
+         
+        //Print total available memory
+        System.out.println("Total Memory:" + runtime.totalMemory() / mb);
+ 
+        //Print Maximum available memory
+        System.out.println("Max Memory:" + runtime.maxMemory() / mb);
 	}
 }
